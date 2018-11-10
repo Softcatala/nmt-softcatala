6382f7ec767d3c2328504fba225b820462bf18ae
Jordi Mas <jmas@softcatala.org>
2018-11-10 17:08:44 +0100

    Translations

--- ApplyToPoFile/run-model-sever.sh
+++ ApplyToPoFile/run-model-sever.sh
@@ -1,1 +1,1 @@
-tensorflow_model_server --port=9000 --enable_batching=true --batching_parameters_file=batching_parameters.txt --model_name=1531713563 --model_base_path=$(realpath ../models)
+tensorflow_model_server --port=9000 --enable_batching=true --batching_parameters_file=batching_parameters.txt --model_name=1532515736 --model_base_path=$(realpath ../models)
