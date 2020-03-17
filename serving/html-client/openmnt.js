808afb4f74e27a2da0ae00f3d835ab1b85d155c9
Jordi Mas <jmas@softcatala.org>
2020-03-17 13:46:33 +0100

    Client

---
+++ serving/html-client/openmnt.js
@@ -0,0 +1,32 @@
+
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );
+        anHttpRequest.send( null );
+    }
+}
+
+function translate_text() {
+
+    text = document.getElementById('source_text').value;
+    url = `http://localhost:8700/translate/?text=${text}`;
+
+    var client = new HttpClient();
+    client.get(url, function(response) {
+        json = JSON.parse(response);
+        element = document.getElementById('translated_text');
+        text = json["translated"];
+        element.innerText = text;
+
+        element = document.getElementById('time_used');
+        text = json["time"];
+        element.innerText = text;
+    });
+}
+
