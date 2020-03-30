ec76ab512c995f4e5df82638ddd1d71168f5fd9a
Jordi Mas <jmas@softcatala.org>
2020-03-30 12:51:25 +0200

    Quality ca

--- quality/run.sh
+++ quality/run.sh
@@ -1,6 +1,8 @@
 mkdir -p translated
 cp ../models/eng-cat/1/assets/*.model .
 python3 ../use-models-tools/model-to-txt.py -m eng-cat -f input/common-sentences.en -t translated/common-sentences.ca
+python3 ../use-models-tools/model-to-txt.py -m eng-cat -f input/twitts.en -t translated/twitts.ca
+
 diff -u reference/common-sentences.ca translated/common-sentences.ca
 
 
