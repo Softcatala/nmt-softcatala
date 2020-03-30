894b29e475a9ec9b7e9ba6d7a15f3821c1fae3b1
Jordi Mas <jmas@softcatala.org>
2020-03-30 16:05:52 +0200

    Changes

--- quality/run.sh
+++ quality/run.sh
@@ -3,6 +3,5 @@ cp ../models/eng-cat/1/assets/*.model .
 python3 ../use-models-tools/model-to-txt.py -m eng-cat -f input/common-sentences.en -t translated/common-sentences.ca
 python3 ../use-models-tools/model-to-txt.py -m eng-cat -f input/twitts.en -t translated/twitts.ca
 
-diff -u reference/common-sentences.ca translated/common-sentences.ca
-
-
+diff -u reference/common-sentences.ca translated/common-sentences.ca > common-sentences.diff
+diff -u reference/twitts.ca translated/twitts.ca > twitts.diff
