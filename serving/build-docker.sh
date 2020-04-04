456bfb9e899215e675b34f0eb2393beaac1df906
Jordi Mas <jmas@softcatala.org>
2020-04-04 13:37:48 +0200

    Decrease size of Docker container

--- serving/build-docker.sh
+++ serving/build-docker.sh
@@ -2,3 +2,4 @@ echo Builds an image using Tensorflow 2.0 and the models at models/eng-cat/
 pushd ..
 docker build -t traductor-eng-cat . -f serving/Dockerfile --no-cache
 popd
+docker image ls | grep traductor-eng-cat
