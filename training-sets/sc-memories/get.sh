2b1efa2c057c8cc35925b27dd61802388781932f
Jordi Mas <jmas@softcatala.org>
2020-04-05 18:13:25 +0200

    Get/Publish

---
+++ training-sets/sc-memories/get.sh
@@ -0,0 +1,2 @@
+wget https://www.softcatala.org/pub/softcatala/opennmt/training-sets/sc-memories/sc-memories.zip
+
---
+++ training-sets/sc-memories/publish.sh
@@ -0,0 +1,3 @@
+zip -r sc-memories.zip raw/*.po *.txt
+scp *.zip  baixades@baixades:/home/baixades/pub/softcatala/opennmt/training-sets/sc-memories/
+
