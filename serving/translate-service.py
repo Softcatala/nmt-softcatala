b3c61bd45c8b3f1c1a99e73db689fef20e6a887b
Jordi Mas <jmas@softcatala.org>
2020-04-15 09:20:38 +0200

    Threading

--- serving/translate-service.py
+++ serving/translate-service.py
@@ -24,20 +24,36 @@ import json
 import datetime
 from opennmt import OpenNMT
 import pyonmttok
-
+from threading import Thread
 
 app = Flask(__name__)
 openNMT_engcat = OpenNMT()
 openNMT_engcat.tokenizer_source = pyonmttok.Tokenizer(mode="none", sp_model_path="en_m.model")
 openNMT_engcat.tokenizer_target = pyonmttok.Tokenizer(mode="none", sp_model_path="ca_m.model")
 
+openNMT_engcat1 = OpenNMT()
+openNMT_engcat1.tokenizer_source = pyonmttok.Tokenizer(mode="none", sp_model_path="en_m.model")
+openNMT_engcat1.tokenizer_target = pyonmttok.Tokenizer(mode="none", sp_model_path="ca_m.model")
+
+openNMT_engcat2 = OpenNMT()
+openNMT_engcat2.tokenizer_source = pyonmttok.Tokenizer(mode="none", sp_model_path="en_m.model")
+openNMT_engcat2.tokenizer_target = pyonmttok.Tokenizer(mode="none", sp_model_path="ca_m.model")
+
+openNMT_engcat3 = OpenNMT()
+openNMT_engcat3.tokenizer_source = pyonmttok.Tokenizer(mode="none", sp_model_path="en_m.model")
+openNMT_engcat3.tokenizer_target = pyonmttok.Tokenizer(mode="none", sp_model_path="ca_m.model")
+
+openNMT_engcat4 = OpenNMT()
+openNMT_engcat4.tokenizer_source = pyonmttok.Tokenizer(mode="none", sp_model_path="en_m.model")
+openNMT_engcat4.tokenizer_target = pyonmttok.Tokenizer(mode="none", sp_model_path="ca_m.model")
+
+
 openNMT_cateng = OpenNMT()
 openNMT_cateng.tokenizer_source = pyonmttok.Tokenizer(mode="none", sp_model_path="ca_m.model")
 openNMT_cateng.tokenizer_target = pyonmttok.Tokenizer(mode="none", sp_model_path="en_m.model")
 
-
-@app.route('/translate/', methods=['GET'])
-def translate_api():
+@app.route('/translate2/', methods=['GET'])
+def translate2_api():
     start_time = datetime.datetime.now()
     text = request.args.get('text')
     languages = request.args.get('languages')
@@ -50,6 +66,52 @@ def translate_api():
         openNMT = openNMT_cateng
 
     translated = openNMT.translate(model_name, text)
+    result = {}
+    result['text'] = text
+    result['translated'] = translated
+    result['time'] = str(datetime.datetime.now() - start_time)
+    return json_answer(json.dumps(result, indent=4, separators=(',', ': ')))
+
+def translate_thread(sentence, openNMT, i, model_name, results):
+    if sentence.strip() == '':
+        results[i] = ''
+    else:
+        results[i] = openNMT.translate(model_name, sentence)
+    print("{0} - {1} -> {2}".format(i, sentence, results[i]))
+
+@app.route('/translate/', methods=['GET'])
+def translate_api():
+    print("Hellou")
+    start_time = datetime.datetime.now()
+    text = request.args.get('text')
+    languages = request.args.get('languages')
+
+    if languages == 'eng-cat':
+        model_name = 'eng-cat'
+        openNMT = openNMT_engcat
+    else:
+        model_name = 'cat-eng'
+        openNMT = openNMT_cateng
+
+    sentences = text.split(".")
+    num_threads = len(sentences)
+    threads = []
+    results = ["" for x in range(num_threads)]
+    for i in range(num_threads):
+        print('Starting thread {0}'.format(i))
+        process = Thread(target=translate_thread, args=[sentences[i], openNMT, i, model_name, results])
+        process.start()
+        threads.append(process)
+
+    for process in threads:
+        process.join()
+
+    print("Done")
+
+    translated = ''
+    for r in results:
+        translated += r + " "
+
     result = {}
     result['text'] = text
     result['translated'] = translated
