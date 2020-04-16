b7efb43ed57b36c69c0e7551403855a59371d622
Jordi Mas <jmas@softcatala.org>
2020-04-16 14:42:07 +0200

    Use optimized tensorflow in prod

--- serving/build-docker.sh
+++ serving/build-docker.sh
@@ -1,5 +1,14 @@
 echo Builds an image using Tensorflow 2.0 and the models at models/eng-cat/
+if [ "$1" = "prod" ];
+    then
+        buildType="prod"
+        cp Dockerfile DockerfileBuild
+    else
+        buildType='dev'
+        grep -v tensorflow-2.1.0 Dockerfile > DockerfileBuild
+fi
+echo "Build type: $buildType"
 pushd ..
-docker build -t traductor-eng-cat . -f serving/Dockerfile
+#docker build -t traductor-eng-cat . -f serving/Dockerfile
 popd
 docker image ls | grep traductor-eng-cat
