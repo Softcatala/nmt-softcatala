02cfb56f364ef97af29dadf82e91e0031c0eee27
Jordi Mas <jmas@softcatala.org>
2020-04-25 21:27:23 +0200

    Email

--- serving/process-batch.py
+++ serving/process-batch.py
@@ -24,6 +24,8 @@ import os
 import datetime
 from batchfiles.batchfiles import *
 import time
+import smtplib
+import ssl
 
 def init_logging(del_logs):
     logfile = 'process-batch.log'
@@ -37,6 +39,25 @@ def init_logging(del_logs):
     logger.addHandler(hdlr)
     logger.setLevel(logging.WARNING)
 
+def send_email():
+    print("Send email start")
+    port = 465  # For SSL
+    context = ssl.create_default_context()
+    
+    sender_email = "jmas@softcatala.org"
+    receiver_email = "jordimash2@gmail.com"
+
+    with smtplib.SMTP_SSL("mail.scnet", port, context=context) as server:
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "multipart test"
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        text = "Prova al fitxer"
+        part1 = MIMEText(text, "plain")
+        server.sendmail(sender_email, receiver_email, message.as_string())
+
+    print("Send email end")
+
 def main():
 
     init_logging(True)    
@@ -45,14 +66,15 @@ def main():
 
     while True:
         print("Starting to process")
-        batchfiles = BatchFile.select().where(BatchFile.done == 0)
+        batchfiles = BatchFile.select().where(BatchFile.done == 1)
         for batchfile in batchfiles:
             print(batchfile.filename)
             cmd = "python3 model-to-txt.py -f {0} -t {1} {2}".format(batchfile.filename, 
                   str(batchfile.filename) + "-ca.txt", batchfile.model)
             logging.debug("Run {0}".format(cmd))
             os.system(cmd)
-            batchfile.done = True
+            send_email()
+#            batchfile.done = True
             batchfile.update()
 
         time.sleep(10*1000)
