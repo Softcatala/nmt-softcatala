47fb4aa56df6cdb773c700b4a43dea6bce67b53c
Jordi Mas <jmas@softcatala.org>
2020-05-08 21:53:06 +0200

    server

--- serving/opennmt-tf/entry-point.sh
+++ serving/opennmt-tf/entry-point.sh
@@ -4,7 +4,6 @@ cp -r /models/eng-cat/1/assets/*.model $TARGET
 cp -r /models/eng-cat/1/assets/model_description.txt $TARGET/model-description-engcat.txt
 cp -r /models/cat-eng/1/assets/model_description.txt $TARGET/model-description-cateng.txt
 
-bash
-#tensorflow_model_server --port=8500 --rest_api_port=8501 --model_name=eng-cat --model_config_file=models.conf 
+tensorflow_model_server --port=8500 --rest_api_port=8501 --model_name=eng-cat --model_config_file=models.conf
 
 
