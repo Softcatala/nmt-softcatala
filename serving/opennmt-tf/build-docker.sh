a80e9b672ca63f8cacbd47d670764cbc1fb6ac78
Jordi Mas <jmas@softcatala.org>
2020-05-08 17:51:34 +0200

    Fixes

--- serving/opennmt-tf/build-docker.sh
+++ serving/opennmt-tf/build-docker.sh
@@ -1,6 +1,6 @@
 pushd ../..
 docker build -t opennmt-tf . -f serving/opennmt-tf/Dockerfile
 popd
-docker image ls | grep opennmt
+docker image ls | grep opennmt-tf
 
 
--- serving/opennmt-tf/entry-point.sh
+++ serving/opennmt-tf/entry-point.sh
@@ -1,3 +1,12 @@
-tensorflow_model_server --port=8500 --rest_api_port=8501 --model_name=eng-cat --model_config_file=models.conf 
+TARGET=/srv/data/models/
+mkdir $TARGET
+cp /models/eng-cat/1/assets/*.model $TARGET
+cp /models/eng-cat/1/assets/model_description.txt  $TARGET/model-description-engcat.txt
+
+cp /models/cat-eng/ models/cat-eng/	$TARGET
+cp /models/cat-eng/1/assets/model_description.txt  $TARGET/model-description-cateng.txt
+
+bash
+#tensorflow_model_server --port=8500 --rest_api_port=8501 --model_name=eng-cat --model_config_file=models.conf 
 
 
