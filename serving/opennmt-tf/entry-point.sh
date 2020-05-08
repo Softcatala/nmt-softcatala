76e5a785cf4f24f246606820a3b01e5e872b6dfa
Jordi Mas <jmas@softcatala.org>
2020-05-08 20:37:14 +0200

    Fix copy model

--- serving/opennmt-tf/entry-point.sh
+++ serving/opennmt-tf/entry-point.sh
@@ -1,10 +1,8 @@
 TARGET=/srv/data/models/
-mkdir $TARGET
-cp /models/eng-cat/1/assets/*.model $TARGET
-cp /models/eng-cat/1/assets/model_description.txt  $TARGET/model-description-engcat.txt
-
-cp /models/cat-eng/ models/cat-eng/	$TARGET
-cp /models/cat-eng/1/assets/model_description.txt  $TARGET/model-description-cateng.txt
+mkdir -p $TARGET
+cp -r /models/eng-cat/1/assets/*.model $TARGET
+cp -r /models/eng-cat/1/assets/model_description.txt $TARGET/model-description-engcat.txt
+cp -r /models/cat-eng/1/assets/model_description.txt $TARGET/model-description-cateng.txt
 
 bash
 #tensorflow_model_server --port=8500 --rest_api_port=8501 --model_name=eng-cat --model_config_file=models.conf 
