ac69fe8468013a8383e1123c527480bdb7b132fc
Jordi Mas <jmas@softcatala.org>
2020-05-08 11:24:08 +0200

    TF container

---
+++ serving/opennmt-tf/entry-point.sh
@@ -0,0 +1,3 @@
+tensorflow_model_server --port=8500 --rest_api_port=8501 --model_name=eng-cat --model_config_file=models.conf 
+
+
---
+++ serving/opennmt-tf/run-docker.sh
@@ -0,0 +1,2 @@
+docker volume create traductor-files
+docker run -v traductor-files:/srv/data -it --rm -p 8500:8500 -p 8501:8501 -p 8700:8700 traductor-eng-cat
