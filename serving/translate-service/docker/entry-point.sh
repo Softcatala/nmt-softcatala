64f0bc2ccd2c8ce9147e8d2f24623d732f8cb116
Jordi Mas <jmas@softcatala.org>
2020-05-16 12:30:50 +0200

    Manage multiple requests

--- serving/translate-service/docker/entry-point.sh
+++ serving/translate-service/docker/entry-point.sh
@@ -1,3 +1,4 @@
 TARGET=/srv/data/files
 mkdir -p $TARGET
-gunicorn translate-service:app -b 0.0.0.0:8700
+# The requests do not requiere CPU since they are waiting from TF, then threads is more appropiated
+gunicorn --workers=2 --threads=16 translate-service:app -b 0.0.0.0:8700
