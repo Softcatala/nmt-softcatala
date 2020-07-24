5af5c4bd764f74c6942353ea4cbfc49b1e6960a4
Jordi Mas <jmas@softcatala.org>
2020-07-24 19:38:09 +0200

    Add file

--- use-models-tools/docker/entry-point.sh
+++ use-models-tools/docker/entry-point.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
+cd /srv/files
 if [ "$FILE_TYPE" = "po" ] ; then
-    cat > input.po
-    python3 model-to-po.py -f input.po  -p /srv/models/tokenizer/ -x /srv/models/ >/dev/null
-    cat input.po-ca.po
+    echo python3 ../model-to-po.py -p /srv/models/tokenizer/ -x /srv/models/  $COMMAND_LINE
+    python3 ../model-to-po.py -p /srv/models/tokenizer/ -x /srv/models/ $COMMAND_LINE
 else
-    cat > input.txt
-    python3 model-to-txt.py -f input.txt -t output.txt -p /srv/models/tokenizer/ -x /srv/models/ >/dev/null
-    cat output.txt
+    python3 ../model-to-txt.py -f input.txt -t output.txt -p /srv/models/tokenizer/ -x /srv/models/
 fi
-
-
+sleep 1m
--- use-models-tools/docker/run-docker.sh
+++ use-models-tools/docker/run-docker.sh
@@ -1,4 +1,4 @@
-#cat ../input.txt | docker run -i use-model-tools --rm use-model-tools
-cat ../input.po | docker run --env FILE_TYPE='po' -i use-model-tools --rm use-model-tools 
-
+DIRECTORY=/home/jordi/sc/OpenNMT/nmt-softcatala/use-models-tools/
+COMMAND_LINE='-f ca.po'
+docker run -it -v $DIRECTORY:/srv/files/ --env FILE_TYPE='po' --env COMMAND_LINE="$COMMAND_LINE" --rm use-model-tools --name use-model-tools
 
