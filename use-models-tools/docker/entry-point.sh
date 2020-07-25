eb481ee829943b66907319332d643f50d058ea17
Jordi Mas <jmas@softcatala.org>
2020-07-25 16:53:11 +0200

    Fix entry point

--- use-models-tools/docker/entry-point.sh
+++ use-models-tools/docker/entry-point.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 cd /srv/files
 if [ "$FILE_TYPE" = "po" ] ; then
-    echo python3 ../model-to-po.py -p /srv/models/tokenizer/ -x /srv/models/  $COMMAND_LINE
     python3 ../model-to-po.py -p /srv/models/tokenizer/ -x /srv/models/ $COMMAND_LINE
 else
-    python3 ../model-to-txt.py -f input.txt -t output.txt -p /srv/models/tokenizer/ -x /srv/models/
+    python3 ../model-to-txt.py -p /srv/models/tokenizer/ -x /srv/models/ $COMMAND_LINE
 fi
-sleep 1m
