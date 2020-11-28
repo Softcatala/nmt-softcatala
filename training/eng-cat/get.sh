e4420fa277ea856277489b6c102c947d116509e8
Jordi Mas <jmas@softcatala.org>
2020-11-28 13:46:49 +0100

    Corpus

--- training/eng-cat/get.sh
+++ training/eng-cat/get.sh
@@ -1,3 +1,4 @@
-wget https://www.softcatala.org/pub/softcatala/opennmt/training-sets/eng-cat/eng-cat.zip
+#wget https://www.softcatala.org/pub/softcatala/opennmt/training-sets/eng-cat/eng-cat.zip
 unzip eng-cat.zip
-
+head -n 3000000 /home/jordi/sc/OpenNMT/datasets/UNPC/unpc-en.txt > unpc-en.txt
+head -n 3000000 /home/jordi/sc/OpenNMT/datasets/UNPC/unpc-ca.txt > unpc-ca.txt
