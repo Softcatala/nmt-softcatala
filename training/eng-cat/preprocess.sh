7b6f6e10af7ce76124e868d02e309db6c6177071
Jordi Mas <jmas@softcatala.org>
2020-12-22 11:08:09 +0100

    Special test

--- training/eng-cat/preprocess.sh
+++ training/eng-cat/preprocess.sh
@@ -6,4 +6,5 @@ mv -f tgt.txt preprocessed/tots-tm.ca
 
 # Generate final src-val-test single files
 python3 ../../data-processing-tools/join-single-file.py
-
+cat src-test1.txt >> src-test.txt
+cat tgt-test1.txt >> tgt-test.txt
