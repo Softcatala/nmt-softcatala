e87ad8b4432551eca43edce63e93874d2d5767a9
Jordi Mas <jmas@softcatala.org>
2020-12-22 15:15:21 +0100

    Fix

--- training/eng-cat/preprocess.sh
+++ training/eng-cat/preprocess.sh
@@ -5,6 +5,7 @@ mv -f src.txt preprocessed/tots-tm.en
 mv -f tgt.txt preprocessed/tots-tm.ca
 
 # Generate final src-val-test single files
+cat src-test1.txt >> preprocessed/tots-tm.en
+cat tgt-test1.txt >> preprocessed/tots-tm.ca
 python3 ../../data-processing-tools/join-single-file.py
-cat src-test1.txt >> src-test.txt
-cat tgt-test1.txt >> tgt-test.txt
+
