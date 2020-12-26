117d72f77fc2cdd7f1bf46fabd32581e444a870b
Jordi Mas <jmas@softcatala.org>
2020-12-26 17:13:49 +0100

    Do not join files

--- training/eng-cat/preprocess.sh
+++ training/eng-cat/preprocess.sh
@@ -5,7 +5,5 @@ mv -f src.txt preprocessed/tots-tm.en
 mv -f tgt.txt preprocessed/tots-tm.ca
 
 # Generate final src-val-test single files
-cat src-test1.txt >> preprocessed/tots-tm.en
-cat tgt-test1.txt >> preprocessed/tots-tm.ca
 python3 ../../data-processing-tools/join-single-file.py
 
