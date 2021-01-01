169df1bef719b908f3f52e65f689e941a12e83f2
Jordi Mas <jmas@softcatala.org>
2021-01-01 18:38:39 +0100

    Fix voc

--- training/eng-cat/voc.sh
+++ training/eng-cat/voc.sh
@@ -1,4 +1,4 @@
 VOCSIZE=50000
 python3 ../sentencepiece-tokenizer.py -v $VOCSIZE
 onmt-build-vocab --from_vocab en_m.model.vocab --save_vocab src-vocab.txt.token
-onmt-build-vocab --from_vocab en_m.model.vocab --save_vocab tgt-vocab.txt.token
+onmt-build-vocab --from_vocab ca_m.model.vocab --save_vocab tgt-vocab.txt.token
