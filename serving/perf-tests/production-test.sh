5be511c8587fb7b71d69440d542987ec5532c9b4
Jordi Mas <jmas@softcatala.org>
2021-01-14 22:28:56 +0100

    Update performance test scripts

--- serving/perf-tests/production-test.sh
+++ serving/perf-tests/production-test.sh
@@ -1,13 +1,14 @@
+URL='https://www.softcatala.org/sc/v2/api/nmt-engcat/translate/'
+#URL=http://127.0.0.1:5000/translate/
+
 echo "** Catalan - English (10 req, 1 at the time)"
-ab -n 10 -c 1 -p  post-data-cat-eng.json -T application/json https://www.softcatala.org/sc/v2/api/nmt-engcat/translate/ | grep '95%'
+ab -n 10 -c 1 -p  post-data-cat-eng.form -T application/x-www-form-urlencoded $URL | grep '95%'
 
 echo "** Catalan - English (50 req, 5 at the time)"
-ab -n 50 -c 5 -p  post-data-cat-eng.json -T application/json https://www.softcatala.org/sc/v2/api/nmt-engcat/translate/ | grep '95%'
+ab -n 50 -c 5 -p  post-data-cat-eng.form -T application/x-www-form-urlencoded $URL | grep '95%'
 
 echo "** English - Catalan (10 req, 1 at the time)" 
-ab -n 10 -c 1 -p  post-data-eng-cat.json -T application/json https://www.softcatala.org/sc/v2/api/nmt-engcat/translate/ | grep '95%'
+ab -n 10 -c 1 -p  post-data-eng-cat.form -T application/x-www-form-urlencoded $URL | grep '95%'
 
 echo "** English - Catalan (50 req, 5 at the time)" 
-ab -n 50 -c 5 -p  post-data-eng-cat.json -T application/json https://www.softcatala.org/sc/v2/api/nmt-engcat/translate/ | grep '95%'
-
-
+ab -n 50 -c 5 -p  post-data-eng-cat.form -T application/x-www-form-urlencoded $URL | grep '95%'
