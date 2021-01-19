7d3996b40e9dcfc76fb202b713c01f346a2bbaf6
Jordi Mas <jmas@softcatala.org>
2021-01-19 13:46:28 +0100

    Model name

--- training/eng-cat/get.sh
+++ training/eng-cat/get.sh
@@ -1,4 +1,4 @@
-#wget https://www.softcatala.org/pub/softcatala/opennmt/training-sets/eng-cat/eng-cat.zip
+wget https://www.softcatala.org/pub/softcatala/opennmt/training-sets/eng-cat/eng-cat.zip
 unzip eng-cat.zip
 head -n 2000000 /home/jordi/sc/OpenNMT/datasets/UNPC/unpc-en.txt > unpc-en.txt
 head -n 2000000 /home/jordi/sc/OpenNMT/datasets/UNPC/unpc-ca.txt > unpc-ca.txt
