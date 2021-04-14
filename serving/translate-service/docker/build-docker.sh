5612da466aa587979165eb364cf9431c7205d491
Jordi Mas <jmas@softcatala.org>
2021-04-14 14:09:35 +0200

    Fix docker

--- serving/translate-service/docker/build-docker.sh
+++ serving/translate-service/docker/build-docker.sh
@@ -1,6 +1,7 @@
 pushd ../../..
 docker build -t translate-service . -f serving/translate-service/docker/Dockerfile
 popd
-cat ../../../models/cat-eng/metadata/model_description.txt  |grep -w "Model description"
-cat ../../../models/eng-cat/metadata/model_description.txt  | grep -w "Model description"
+cat ../../../models/cat-eng/metadata/model_description.txt | grep -w "Model description"
+cat ../../../models/eng-cat/metadata/model_description.txt | grep -w "Model description"
+cat ../../../models/deu-cat/metadata/model_description.txt | grep -w "Model description"
 docker image ls | grep translate-service
