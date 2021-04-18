09576183407c5343c5213a0dacbab8e0cad53a69
Jordi Mas <jmas@softcatala.org>
2021-04-18 10:25:01 +0200

    Better delete script

--- training/delete.sh
+++ training/delete.sh
@@ -2,3 +2,4 @@
 echo Deletes unnecessary files before training
 sudo rm -r -f /var/cache/apt/archives/
 sudo rm -r -f /usr/local/cuda-11.0
+sudo rm -f -r /home/ubuntu/anaconda3/envs/ # 40Gb of python envs
