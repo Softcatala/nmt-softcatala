9a12ca5f612a276dc519def6fdcb45a9ab8f9d5a
Jordi Mas <jmas@softcatala.org>
2021-04-18 15:43:08 +0200

    Show image

--- serving/translate-service/docker/build-docker.sh
+++ serving/translate-service/docker/build-docker.sh
@@ -4,4 +4,5 @@ popd
 cat ../../../models/cat-eng/metadata/model_description.txt | grep -e "Model description" -e "Date" && echo -e "\r"
 cat ../../../models/eng-cat/metadata/model_description.txt | grep -e "Model description" -e "Date" && echo -e "\r"
 cat ../../../models/deu-cat/metadata/model_description.txt | grep -e "Model description" -e "Date" && echo -e "\r"
+cat ../../../models/cat-deu/metadata/model_description.txt | grep -e "Model description" -e "Date" && echo -e "\r"
 docker image ls | grep translate-service
