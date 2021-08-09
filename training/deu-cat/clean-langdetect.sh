88bd42484291694c8433b8c54818be7097e9604e
Jordi Mas <jmas@softcatala.org>
2021-08-09 20:16:44 +0200

    Unnecessary files

--- training/deu-cat/clean-langdetect.sh
+++
@@ -1,3 +0,0 @@
-pushd clean-langdetect
-python3 ../../../data-processing-tools/clean-wikimatrix.py -s ccaligned.1000 -t ccaligned-clean
-popd
--- training/deu-cat/clean-lt.sh
+++
@@ -1,3 +0,0 @@
-pushd clean-lt
-python3 ../../../data-processing-tools/clean-lt.py -s ccaligned.1000 -t ccaligned-clean
-popd
