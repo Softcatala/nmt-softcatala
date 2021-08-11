81581e0976f2d65fef68ac2184b8b19784b4f206
Jordi Mas <jmas@softcatala.org>
2021-08-11 18:54:46 +0200

    Use symbolic links for cat-deu, instead of copying the configuration

--- training/cat-deu/bleu.sh
+++ training/cat-deu/bleu.sh
@@ -1,5 +1,1 @@
-onmt-main --config data.yml --auto_config infer --features_file src-test.txt.token > predictions.txt.token
-perl ../multi-bleu.perl tgt-test.txt.token < predictions.txt.token 
-python3 ../sentencepiece-bleu.py
-perl ../multi-bleu.perl tgt-test.txt < predictions.txt > bleu.txt
-cat bleu.txt
+../eng-cat/bleu.sh
