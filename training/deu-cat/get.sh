047099fa0d8fe6f974103dc25b8e439f9ed7b0c3
Jordi Mas <jmas@softcatala.org>
2021-08-22 19:10:04 +0200

    Fix deu-cat get corpus

--- training/deu-cat/get.sh
+++ training/deu-cat/get.sh
@@ -1,5 +1,6 @@
-git clone https://github.com/Softcatala/de-ca-corpus.git
-cd de-ca-corpus/data
+git clone https://github.com/Softcatala/parallel-catalan-corpus corpus-raw
+cd corpus-raw/deu-cat/
 7z -y x "*.7z"
-unzip *.zip
+unzip europarl.de-ca.ca.zip
+unzip europarl.de-ca.de.zip
 
