91fc0713f9018b2054a00541a4d1016006ad8f6c
Jordi Mas <jmas@softcatala.org>
2021-08-28 09:25:17 +0200

    Remove legacy scripts

--- training/sc-memories/get.sh
+++
@@ -1,2 +0,0 @@
-wget https://www.softcatala.org/pub/softcatala/opennmt/training-sets/sc-memories/sc-memories.zip
-
--- training/sc-memories/publish.sh
+++
@@ -1,3 +0,0 @@
-zip -r sc-memories.zip raw/*.po *.txt
-scp *.zip  baixades@baixades:/home/baixades/pub/softcatala/opennmt/training-sets/sc-memories/
-
