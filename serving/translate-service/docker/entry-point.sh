7569de44b2db6323339c3c833fa34e5fae0f68f2
Jordi Mas <jmas@softcatala.org>
2022-04-17 09:37:21 +0200

    Decrease number of threads to lower memory usage

--- serving/translate-service/docker/entry-point.sh
+++ serving/translate-service/docker/entry-point.sh
@@ -3,5 +3,5 @@ SAVED_TEXTS=/srv/data/saved/
 mkdir -p $UPLOAD_FOLDER
 mkdir -p $SAVED_TEXTS
 
-# The requests do not requiere CPU since they are waiting from TF, then threads is more appropiated
-gunicorn --workers=2 --graceful-timeout 90 --timeout 90 --threads=16 translate-service:app -b 0.0.0.0:8700
+# The requests do not requiere CPU since they are waiting from CTranslate2, then threads is more appropiated
+gunicorn --workers=2 --graceful-timeout 90 --timeout 90 --threads=4 translate-service:app -b 0.0.0.0:8700
