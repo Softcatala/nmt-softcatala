f482b95f9e50feb4cf892cfb0a069a4bb9bd4979
Jordi Mas <jmas@softcatala.org>
2023-12-21 15:48:48 +0100

    Fix

--- use-models-tools/docker/entry-point.sh
+++ use-models-tools/docker/entry-point.sh
@@ -2,12 +2,6 @@
 
 #COMMAND_LINE="-f flores200.eng -t output.cat -m eng-cat"
 
-COMMAND_LINE="-f cat.txt -t output.cat -m eng-cat"
 
 cd /srv/flores
-if [ "$FILE_TYPE" = "po" ] ; then
-    model_to_po -x /srv/models/ $COMMAND_LINE
-else
-    model_to_txt -x /srv/models/ $COMMAND_LINE
-fi
-
+whisper-ctranslate2 15GdH1.mp3  --model medium --device $DEVICE
