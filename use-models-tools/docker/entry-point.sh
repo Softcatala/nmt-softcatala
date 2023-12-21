f7b22727aaa887df2f940cdd017d3807fb6d2af6
Jordi Mas <jmas@softcatala.org>
2023-12-21 14:40:35 +0100

    flores

--- use-models-tools/docker/entry-point.sh
+++ use-models-tools/docker/entry-point.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-cd /srv/files
+
+COMMAND_LINE="-f flores200.eng -t output.cat -m eng-cat"
+
+cd /srv/flores
 if [ "$FILE_TYPE" = "po" ] ; then
     model_to_po -x /srv/models/ $COMMAND_LINE
 else
