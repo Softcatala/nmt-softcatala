8475ced1ccbd73a3e0063862ec76391d0f4c486f
Jordi Mas <jmas@softcatala.org>
2023-12-27 20:40:32 +0100

    Fix time counter

--- use-models-tools/docker/entry-point.sh
+++ use-models-tools/docker/entry-point.sh
@@ -6,4 +6,4 @@
 #docker run --env "DEVICE=cpu" --env "COMPUTE_TYPE=int8" --env "DEVICE_INDEX=0"   --gpus 0  -it registry.softcatala.org/github/nmt-softcatala/use-models-tools:test-whisper
 
 cd /srv/flores
-/usr/bin/time -f %U whisper-ctranslate2 15GdH1.mp3  --model medium --threads 8 --device $DEVICE --compute_type $COMPUTE_TYPE --device_index $DEVICE_INDEX
+/usr/bin/time -f %e whisper-ctranslate2 15GdH1.mp3  --model medium --threads 8 --device $DEVICE --compute_type $COMPUTE_TYPE --device_index $DEVICE_INDEX
