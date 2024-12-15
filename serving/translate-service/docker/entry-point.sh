13613126e424ad1efa1286602e4659a9d05fdc3b
Jordi Mas <jmas@softcatala.org>
2024-12-15 20:17:14 +0100

    Set larger size for GET requests

--- serving/translate-service/docker/entry-point.sh
+++ serving/translate-service/docker/entry-point.sh
@@ -4,4 +4,4 @@ mkdir -p $UPLOAD_FOLDER
 mkdir -p $SAVED_TEXTS
 
 # The requests do not requiere CPU since they are waiting from CTranslate2, then threads is more appropiated
-gunicorn --workers=2 --graceful-timeout 90 --timeout 90 --threads=4 translate-service:app -b 0.0.0.0:8700
+gunicorn  --limit-request-line 8192 --workers=2 --graceful-timeout 90 --timeout 90 --threads=4 translate-service:app -b 0.0.0.0:8700
